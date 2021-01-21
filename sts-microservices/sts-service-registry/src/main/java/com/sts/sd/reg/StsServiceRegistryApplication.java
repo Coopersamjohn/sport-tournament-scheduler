89aaa8b8a882f5ff9cffecbb5d7cd6fa0a2e5a92
coopersamjohn <coopersamjohn@gmail.com>
2021-01-20 19:30:43 -0600

    STS-1. Initial setup

---
+++ sts-microservices/sts-service-registry/src/main/java/com/sts/sd/reg/StsServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.sts.sd.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ sts-microservices/sts-service-registry/src/test/java/com/sts/sd/reg/StsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.sd.reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
